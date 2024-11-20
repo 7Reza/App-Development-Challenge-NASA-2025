3ca79eb47f821553b18d6ecdf60cad835503b1dc
7Reza <mohamadrezakahanali@gmail.com>
2024-11-20 09:50:48 -0500

    Update redraw_rectangel.py

--- redraw_rectangel.py
+++ redraw_rectangel.py
@@ -1,22 +1,4 @@
-from manim import * 
-import pandas as pd
-
-df = pd.read_excel("fy25-adc-high-school-data.xlsx", sheet_name="1")  # Replace with actual file name if needed
-B_n_values = df['B_n'].to_list()
-int_B_n_values = [int(x) for x in B_n_values]
-
-
-class rectangle(Scene):
-    def construct(self):
-        for i in range(10):
-            k=0
-            rect = always_redraw(Rectangle(width=.2, height=(B_n_values[k]/1000000)).to_edge(UL))
-            k+=1
-            self.play(Create(rect))
-
-
-
-from manim import * 
+from manim import *
 import pandas as pd
 
 # Load data from Excel
@@ -25,9 +7,13 @@ B_n_values = df['B_n'].to_list()
 
 class rectangle(Scene):
     def construct(self):
-        for i in range(10):
-            # Create a new rectangle each time with the i-th value from B_n_values
-            rect = Rectangle(width=0.2, height=B_n_values[i] / 1000000).to_edge(UL)
-            
-            # Play animation to create the rectangle
-            self.play(Create(rect))
+        # Initialize the rectangle with the first height value
+        rect = Rectangle(width=0.2, height=B_n_values[0] / 1000000).to_edge(UL)
+
+        # Play the initial rectangle creation
+        self.play(Create(rect))
+
+        # Update the rectangle in the loop
+        for i in range(1, 10):
+            # Animate the update of the rectangle's height
+            self.play(rect.animate.set_height(B_n_values[i] / 1000000))
